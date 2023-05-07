38c2707d4dc2dcf4f753bdea1119446055141b48
Lucas Luann <lucasluann10@gmail.com>
2023-05-07 12:21:45 -0400

    Construindo com Future Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?key=b8ff9e23";
 
 void main() async {
-  http.Response response = await http.get(Uri.parse(request));
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
+  print(await getData());
 
-  runApp(MaterialApp(home: Container()));
+  runApp(MaterialApp(home: Home()));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor de Moeda \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text(
+                    "Carregando Dados...",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                      child: Text(
+                    "Erro ao Carregar Dados... :(",
+                    style: TextStyle(
+                      color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ));
+                } else {
+                  return Container(
+                    color: Colors.green,
+                  );
+                }
+            }
+          }),
+    );
+  }
 }
